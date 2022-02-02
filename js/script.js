ad9c1e9e948ecb24e52703bd9d06a376ceedcb97
alexanderuck <80569120+alexanderuck@users.noreply.github.com>
2022-02-02 11:52:23 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Menu burger
+    let header__burger = document.querySelector('.header__burger');
+    let header_menu = document.querySelector('.header__menu');
+    let back = document.querySelector('body');
+    let header__items = document.querySelector('.header__items');
+
+    header__burger.addEventListener('click', function () {
+        header__burger.classList.toggle('active');
+        header_menu.classList.toggle('active');
+        back.classList.toggle('lock');
+    });
+
+    header__items.addEventListener('click', function () {
+        header__items.classList.remove('active');
+        back.classList.toggle('lock');
+    });
+    //  /.Menu burger
+
+
+    // Button to Top
+    let btn = document.querySelector('#toTop');
+    window.addEventListener('scroll', function () {
+        // Если прокрутили дальше 599px, показываем кнопку
+        if (pageYOffset > 100) {
+            btn.classList.add('show');
+            // Иначе прячем
+        } else {
+            btn.classList.remove('show');
+        }
+    });
+
+    // При клике прокручиываем на самый верх
+    btn.onclick = function (click) {
+        click.preventDefault();
+        scrollTo(0, 400);
+    }
+    // /.Button to Top
+});
+
+function scrollTo(to, duration = 700) {
+    const
+        element = document.scrollingElement || document.documentElement,
+        start = element.scrollTop,
+        change = to - start,
+        startDate = +new Date(),
+        // t = current time
+        // b = start value
+        // c = change in value
+        // d = duration
+        easeInOutQuad = function (t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        },
+        animateScroll = function () {
+            const currentDate = +new Date();
+            const currentTime = currentDate - startDate;
+            element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
+            if (currentTime < duration) {
+                requestAnimationFrame(animateScroll);
+            }
+            else {
+                element.scrollTop = to;
+            }
+        };
+    animateScroll();
+}
